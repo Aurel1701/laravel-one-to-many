93cf882753fddb2ab69a5f5700256b5bac516175
Aurel1701 <gantzfan96@gmail.com>
2022-06-27 08:54:56 +0200

    edit Controller

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'cover', 'body'];
+    protected $fillable = ['title', 'cover', 'body', 'category_id'];
 
     
     /**
